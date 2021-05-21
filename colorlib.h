0f326751eb3ecdf2f0c4a6a84529311b412240f3
Parbhat Sharma <83265789+parbhat-cpp@users.noreply.github.com>
2021-05-21 21:34:17 +0530

    Add files via upload

---
+++ colorlib.h
@@ -0,0 +1,37 @@
+class color
+{
+	public:
+	std::string RESET="\033[0m";
+	std::string BLACK="\033[30m";
+	std::string RED="\033[31m";
+	std::string GREEN="\033[32m";
+	std::string YELLOW="\033[33m";
+	std::string BLUE="\033[34m";
+	std::string MAGENTA="\033[35m";
+	std::string CYAN="\033[36m";
+	std::string WHITE="\033[37m";
+	std::string BOLDBLACK="\033[1m\033[30m";
+	std::string BOLDRED="\033[1m\033[31m";
+	std::string BOLDGREEN="\033[1m\033[32m";
+	std::string BOLDYELLOW="\033[1m\033[33m";
+	std::string BOLDBLUE="\033[1m\033[34m";
+	std::string BOLDMAGENTA="\033[1m\033[35m";
+	std::string BOLDCYAN="\033[1m\033[36m";
+	std::string BOLDWHITE="\033[1m\033[37m";
+	std::string FILLBLACK="\033[3m\033[30m";
+	std::string FILLRED="\033[3m\033[31m";
+	std::string FILLGREEN="\033[3m\033[32m";
+	std::string FILLYELLOW="\033[3m\033[33m";
+	std::string FILLBLUE="\033[3m\033[34m";
+	std::string FILLMAGENTA="\033[3m\033[35m";
+	std::string FILLCYAN="\033[3m\033[36m";
+	std::string FILLWHITE="\033[3m\033[37m";
+	std::string UNDERLINEBLACK="\033[4m\033[30m";
+	std::string UNDERLINERED="\033[4m\033[31m";
+	std::string UNDERLINEGREEN="\033[4m\033[32m";
+	std::string UNDERLINEYELLOW="\033[4m\033[33m";
+	std::string UNDERLINEBLUE="\033[4m\033[34m";
+	std::string UNDERLINEMAGENTA="\033[4m\033[35m";
+	std::string UNDERLINECYAN="\033[4m\033[36m";
+	std::string UNDERLINEWHITE="\033[4m\033[37m";
+}TXT;
